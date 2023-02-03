8246d016d41eff536773539b97df7ed8974284dd
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-02-03 14:45:20 +0000

    Add map to results.html

---
+++ assets/javascript/map-and-route.js
@@ -0,0 +1,9 @@
+
+var map = new maplibregl.Map({
+    container: 'my-map',
+    style:
+    'https://api.maptiler.com/maps/streets/style.json?key=' + mapKey,
+    center: [-0.3758, 51.51237],
+    zoom: 11
+});
+     
