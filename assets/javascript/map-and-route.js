df15b089e9077c2f2e78565a5aec0dc3974c850a
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-02-03 15:04:44 +0000

    Display route info on results.html

--- assets/javascript/map-and-route.js
+++ assets/javascript/map-and-route.js
@@ -23,7 +23,53 @@ const toWaypoint = [eventLong, eventLat]; // longitude, latutude
 const toWaypointMarker = new maplibregl.Marker().setLngLat(toWaypoint)
   .setPopup(new maplibregl.Popup().setText()).addTo(map);
 
+let routeData;
+let routeStepsData;
+let journeyObjData;
+let instructionsData = [];
+let journeyObj = [];
+
 fetch(`https://api.geoapify.com/v1/routing?waypoints=lonlat:${fromWaypoint.join(",")}|lonlat:${toWaypoint.join(",")}&mode=drive&details=route_details&apiKey=${geoapifyKey}`).then(res => res.json()).then(routeResult => {
     routeData = routeResult;
     console.log(routeData);
+    const steps = [];
+    const instructions = [];
+  
+    routeData.features[0].properties.legs.forEach((leg, legIndex) => {
+      const legGeometry = routeData.features[0].geometry.coordinates[legIndex];
+      leg.steps.forEach((step, index) => {
+        if (step.instruction) {
+          journeyObj.push({
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": legGeometry[step.from_index]
+            },
+            properties: {
+                text: step.instruction.text
+            }
+          });
+        }
+      });
+    });
+    routeInfo(journeyObj);
 });
+
+function routeInfo() {
+    console.log(journeyObj);
+    for (let i = 0; i < journeyObj.length; i++) {
+      instructionsData.push(journeyObj[i].properties.text);
+    }
+  
+    let routeInfoHeader = $(` <h3 class="px-3 w-100 text-center"> Route Information </h3> `);
+    $('#routeInfo').append(routeInfoHeader) 
+  
+    for (let i = 0; i < instructionsData.length; i++) {
+      let routeInfoCard = $(
+      `
+        <p class="card-text"> ${i+1}. ${instructionsData[i]}</p>
+      `
+    );
+    $('#routeInfo').append(routeInfoCard) 
+    }
+  }
